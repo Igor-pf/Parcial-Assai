92e57558af8aac9aa8c4ea48f62693952c3ecf79
Igor Pereira Formighieri <igorpereira1069@gmail.com>
2022-08-15 18:38:38 -0300

    Inserido a licença de distribuição

--- Parcial.py
+++ Parcial.py
@@ -1,3 +1,20 @@
+#    Copyright (C) 2022  Igor Pereira Formighieri <igorpereira1069@gmail.com>
+
+
+#    Este programa é um software livre: você pode redistribuí-lo e/ou
+#    modificá-lo sob os termos da Licença Pública Geral GNU, conforme
+#    publicado pela Free Software Foundation, seja a versão 3 da Licença
+#    ou (a seu critério) qualquer versão posterior.
+
+#    Este programa é distribuído na esperança de que seja útil,
+#    mas SEM QUALQUER GARANTIA; sem a garantia implícita de
+#    COMERCIALIZAÇÃO OU ADEQUAÇÃO A UM DETERMINADO PROPÓSITO. Veja a
+#    Licença Pública Geral GNU para obter mais detalhes.
+
+#    Você deve ter recebido uma cópia da Licença Pública Geral GNU
+#    junto com este programa. Se não, veja <https://www.gnu.org/licenses/>.
+
+
 from asyncio.windows_events import NULL
 from fileinput import close
 from webbrowser import get
